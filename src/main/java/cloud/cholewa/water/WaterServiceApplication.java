c7ca0b585a7d433fce527b4ea7a4562cba7cfaf4
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2025-06-01 12:53:12 +0200

    initial commit

---
+++ src/main/java/cloud/cholewa/water/WaterServiceApplication.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.water;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaterServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cloud/cholewa/water/WaterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.water;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
