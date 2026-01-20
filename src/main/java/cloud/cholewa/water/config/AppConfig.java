203778ab2a54c1687736548a7cba8f918cad7e5c
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2026-01-20 18:47:28 +0100

    add HotWaterSensorCronTest; configure custom WebClient with connection and timeout settings

--- src/main/java/cloud/cholewa/water/config/AppConfig.java
+++ src/main/java/cloud/cholewa/water/config/AppConfig.java
@@ -1,8 +1,12 @@
 package cloud.cholewa.water.config;
 
+import io.netty.channel.ChannelOption;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
 
 @Configuration
 public class AppConfig {
@@ -13,7 +17,22 @@ public class AppConfig {
     }
 
     @Bean
-    WebClient webClient(final WebClient.Builder webClientBuilder) {
-        return webClientBuilder.build();
+    ConnectionProvider waterConnectionProvider() {
+        return ConnectionProvider.builder("waterConnectionProvider")
+            .maxConnections(50)
+            .build();
+    }
+
+    @Bean
+    HttpClient waterHttpClient(final ConnectionProvider waterConnectionProvider) {
+        return HttpClient.create(waterConnectionProvider)
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000);
+    }
+
+    @Bean
+    WebClient webClient(final WebClient.Builder webClientBuilder, final HttpClient waterHttpClient) {
+        return webClientBuilder
+            .clientConnector(new ReactorClientHttpConnector(waterHttpClient))
+            .build();
     }
 }
---
+++ src/test/java/cloud/cholewa/water/hot/cron/HotWaterSensorCronTest.java
@@ -0,0 +1,32 @@
+package cloud.cholewa.water.hot.cron;
+
+import cloud.cholewa.water.hot.service.HotWaterService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Answers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class HotWaterSensorCronTest {
+
+    @Mock(answer = Answers.RETURNS_SMART_NULLS)
+    private HotWaterService hotWaterService;
+
+    @InjectMocks
+    private HotWaterSensorCron sut;
+
+    @Test
+    void should_update_hot_water_temperature() {
+        when(hotWaterService.handleHotWaterUpdate()).thenReturn(Mono.empty());
+
+        sut.updateHotWaterTemperature()
+            .as(StepVerifier::create)
+            .verifyComplete();
+    }
+}
